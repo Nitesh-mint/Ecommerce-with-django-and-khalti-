cb9df3d128cb48aec6112bc2320cb1951c4fb2ca
Nitesh-mint <nitesh.raya058@gmail.com>
2023-03-11 11:47:29 +0545

    added out of stock display

--- store/views.py
+++ store/views.py
@@ -21,3 +21,15 @@ def store(request, category_slug=None):
             'product_count' : product_count,
         }
     return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        # category ko slug ma category_slug ko value pass huncha and product ko slug ma product_slug pass huncha ra item return huncha
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'single_product': single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
