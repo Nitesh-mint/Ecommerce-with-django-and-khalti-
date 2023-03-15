563a439cb2466fc3b2566e4be19df87e84687ee5
Nitesh-mint <nitesh.raya058@gmail.com>
2023-03-15 18:00:22 +0545

    initial commit

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category 
+from carts.models import CartItem
+from carts.views import _cart_id
+
+from django.http import HttpResponse
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -26,10 +30,12 @@ def product_detail(request, category_slug, product_slug):
     try:
         # category ko slug ma category_slug ko value pass huncha and product ko slug ma product_slug pass huncha ra item return huncha
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
     except Exception as e:
         raise e
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
