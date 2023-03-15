d252a5f14314e89f5b6df5c2f66d96717842e653
Nitesh-mint <nitesh.raya058@gmail.com>
2023-03-15 17:59:34 +0545

    intial commit

--- hamro_dokan/views.py
+++ hamro_dokan/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from carts.models import CartItem
 from store.models import Product
+from carts.views import _cart_id
+
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
+    # single_product = 
+    # in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=)
     context = {
         'products' : products,
     }
