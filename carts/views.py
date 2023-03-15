4eeea7905a6d27149a0aa68f20262c6e4f93d0c8
Nitesh-mint <nitesh.raya058@gmail.com>
2023-03-15 17:59:21 +0545

    initial commit

--- carts/views.py
+++ carts/views.py
@@ -63,6 +63,9 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+
+            # if quantity > Product.stock:
+            #     exit
         tax = (2* total) / 100
         grand_total = total + tax
     except ObjectDoesNotExist:
