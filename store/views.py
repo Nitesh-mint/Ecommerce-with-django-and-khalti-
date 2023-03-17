7f01e13a325ce86137b9e7bb654ce8bd37295cbc
Nitesh-mint <nitesh.raya058@gmail.com>
2023-03-17 21:43:21 +0545

    fixed a bug in cart page

--- store/views.py
+++ store/views.py
@@ -22,7 +22,7 @@ def store(request, category_slug=None):
         products = Product.objects.all().filter(category=categories, is_available=True)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 1)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
