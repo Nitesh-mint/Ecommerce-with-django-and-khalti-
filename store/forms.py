25a491902de50e467211380100b773e4230aa686
Nitesh-mint <nitesh.raya058@gmail.com>
2023-05-15 20:18:49 +0545

    added comment system

--- store/forms.py
+++ store/forms.py
@@ -2,6 +2,7 @@ from .models import ReviewRating
 from django import forms
 
 class ReviewForm(forms.ModelForm):
-    model = ReviewRating
-    fields = ['subject','review','rating']
+    class Meta:
+        model = ReviewRating
+        fields = ['subject','review','rating']
     
--- store/views.py
+++ store/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect , get_object_or_404  
-from .models import Product
+from .models import Product, ReviewRating
 from category.models import Category 
 from carts.models import CartItem
 from carts.views import _cart_id
+from orders.models import OrderProduct
 
 from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.db.models import Q #to make OR operator possible in searching product in line 57
@@ -11,6 +12,7 @@ from django.contrib import messages
 
 
 from django.http import HttpResponse
+from .forms import ReviewForm
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -43,12 +45,20 @@ def product_detail(request, category_slug, product_slug):
         # category ko slug ma category_slug ko value pass huncha and product ko slug ma product_slug pass huncha ra item return huncha
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
+        reviews = ReviewRating.objects.filter(product__id=single_product.id)
     except Exception as e:
         raise e
     
+    if request.user.is_authenticated:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id = single_product.id).exists()
+    else:
+        orderproduct = None
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
+        'reviews': reviews,
+        'orderproduct':orderproduct,
     }
     return render(request, 'store/product_detail.html', context)
 
@@ -67,3 +77,27 @@ def search(request):
     } 
 
     return render(request, 'store/store.html', context)
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews) #instance checks if there is already a review of this product
+            form.save()
+            messages.success(request, "Thank you for updating the review.")
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.review = form.cleaned_data['review']
+                data.rating = form.cleaned_data['rating']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, "Thank you for your Review.")
+                return redirect(url)
